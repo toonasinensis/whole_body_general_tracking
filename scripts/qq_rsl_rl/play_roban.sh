90e7d1bfe02f2feef783b193193f88bbde5660b5
Wuying Created Local Users <zhangqiqi@9jdtbh65nbbas8b.CN-SHENZHEN-E-198-1.WUYING.LOCAL>
2026-04-22 23:46:50 +0800

    update gitignore

--- scripts/qq_rsl_rl/play_roban.sh
+++ scripts/qq_rsl_rl/play_roban.sh
@@ -3,9 +3,9 @@
 python scripts/qq_rsl_rl/play.py \
 --task=Tracking-Flat-RobanS22-v0 \
 --num_envs=16 \
---resume_path=logs/rsl_rl/roban_flat/0419_squat_kept_offee/model_17000.pt \
+--resume_path=logs/rsl_rl/roban_flat/2026-04-21_16-17-55_motions_all_kept/model_17300.pt \
 --motion_file=data/roban_motions \
---motion_file_txt=data/roban_motions_list/motions_squat_kept.txt \
+--motion_file_txt=data/roban_motions_list/motions_crawl_kept.txt \
 --max_motion_num=160 \
 # --resume_path=logs/rsl_rl/roban_flat/0419_squat_kept/model_17000.pt \
 # --resume_path=logs/rsl_rl/roban_flat/0419_main_kept/model_16300.pt \
