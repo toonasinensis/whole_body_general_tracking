c469ee2a61bbf40aa311b58d9fd8e10d7f25d78e
AbelZHANG <zhangsevenseven@gmail.com>
2026-05-09 16:49:42 +0800

    enhance adaptive sampling mechanism

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/mdp/commands.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/mdp/commands.py
@@ -329,6 +329,7 @@ class MotionCommand(CommandTerm):
         self._future_step_offsets = torch.tensor(future_step_num, device=self.device, dtype=torch.long)
 
         # If requested, bind each env to a fixed motion id (env i -> motion i).
+        # TODO think a more elegant way to fixing motion to environments
         if bool(getattr(self.cfg, "fixed_eval_motion_ids", False)) and bool(getattr(self.cfg, "eval_mode", False)):
             self._setup_fixed_eval_motion_assignment()
 
@@ -607,8 +608,28 @@ class MotionCommand(CommandTerm):
         self.metrics["failures_max_over_uniform"][:] = (
             self.bin_failed_count.max() * self.bin_count / (self.bin_failed_count.sum() + 1e-8)
         )
+        # original sampling mechanism
+        # clipped_bin_failed_count = torch.clamp(
+        #     self.bin_failed_count, max=self.cfg.failure_cap_beta * self.bin_failed_count.mean()
+        # )
+        # sampling_probabilities = torch.nn.functional.pad(
+        #     clipped_bin_failed_count.unsqueeze(0).unsqueeze(0),
+        #     (0, self.cfg.adaptive_kernel_size - 1),
+        #     mode="replicate",
+        # )
+        # sampling_probabilities = torch.nn.functional.conv1d(sampling_probabilities, self.kernel.view(1, 1, -1)).view(-1)
+        # sampling_probabilities = sampling_probabilities / (sampling_probabilities.sum() + 1e-8)
+        # sampling_probabilities = torch.clamp(
+        #     sampling_probabilities, max=self.cfg.failure_cap_beta * sampling_probabilities.mean()
+        # )
+        # sampling_probabilities = (1 - self.cfg.adaptive_uniform_ratio) * sampling_probabilities + (
+        #     self.cfg.adaptive_uniform_ratio
+        # ) / float(self.bin_count)
+        
+        # hierarchical sampling mechanism
+        # pre-compute sampling probs
         clipped_bin_failed_count = torch.clamp(
-            self.bin_failed_count, max=self.cfg.failure_cap_beta * self.bin_failed_count.mean()
+            self.bin_failed_count, max=self.cfg.failure_most_hard_cap_beta * self.bin_failed_count.mean()
         )
         sampling_probabilities = torch.nn.functional.pad(
             clipped_bin_failed_count.unsqueeze(0).unsqueeze(0),
@@ -616,13 +637,29 @@ class MotionCommand(CommandTerm):
             mode="replicate",
         )
         sampling_probabilities = torch.nn.functional.conv1d(sampling_probabilities, self.kernel.view(1, 1, -1)).view(-1)
-        sampling_probabilities = sampling_probabilities / (sampling_probabilities.sum() + 1e-8)
-        sampling_probabilities = torch.clamp(
+        sampling_probabilities = sampling_probabilities / (sampling_probabilities.sum() + 1e-12)
+
+        # middle hard motion clip
+        sampling_probabilities_middle_hard = torch.clamp(
             sampling_probabilities, max=self.cfg.failure_cap_beta * sampling_probabilities.mean()
         )
-        sampling_probabilities = (1 - self.cfg.adaptive_uniform_ratio) * sampling_probabilities + (
-            self.cfg.adaptive_uniform_ratio
-        ) / float(self.bin_count)
+        sampling_probabilities_middle_hard = sampling_probabilities_middle_hard / (
+            sampling_probabilities_middle_hard.sum() + 1e-12
+        )
+
+        # most hard motion clip
+        sampling_probabilities_most_hard = torch.clamp(
+            sampling_probabilities, max=self.cfg.failure_most_hard_cap_beta * sampling_probabilities.mean()
+        )
+        sampling_probabilities_most_hard = sampling_probabilities_most_hard / (
+            sampling_probabilities_most_hard.sum() + 1e-12
+        )
+
+        # final sampling probabilities
+        sampling_probabilities = self.cfg.motion_ratio[0] * (1 / float(self.bin_count)) \
+            + self.cfg.motion_ratio[1] * sampling_probabilities_middle_hard \
+            + self.cfg.motion_ratio[2] * sampling_probabilities_most_hard
+
         return sampling_probabilities
 
     def _get_bin_global_frame_range(self, bin_index: int) -> tuple[int, int]:
@@ -802,7 +839,7 @@ class MotionCommand(CommandTerm):
         self.metrics["sampling_top1_prob_mean"][:] = sampling_probabilities.mean()
         self.metrics["sampling_top1_prob_min"][:] = sampling_probabilities.min()
         self.metrics["num_concentrate_bins"][:] = (
-            sampling_probabilities > 150 * (sampling_probabilities.mean())
+            sampling_probabilities > self.cfg.failure_most_hard_cap_beta * 0.5 * (sampling_probabilities.mean())
         ).sum()  # 计算超过平均值10倍的数目
 
     def _resample_command(self, env_ids: Sequence[int]):
@@ -865,8 +902,6 @@ class MotionCommand(CommandTerm):
             torch.cat([root_pos[env_ids], root_ori[env_ids], root_lin_vel[env_ids], root_ang_vel[env_ids]], dim=-1),
             env_ids=env_ids,
         )
-        # TODO: 切换动作文件时是否需要清空历史Observation
-        # NOTE: what about the historical observation when resetting the environment?
 
     def _update_command(self):
         """
@@ -874,7 +909,9 @@ class MotionCommand(CommandTerm):
         """
         self.command_step_count += 1
         self.local_time_steps += 1
-        env_ids = torch.where(self.local_time_steps >= self.frame_end_per_env)[0]
+        # NOTE why not padding with the last frame if exceeding the frame count ?
+        env_ids = torch.where(self.local_time_steps >= self.frame_end_per_env - self.cfg.max_future_step)[0]  # 防止溢出
+
         # Count completed cycles only on real end-of-clip events during stepping.
         if bool(getattr(self.cfg, "fixed_eval_motion_ids", False)) and bool(getattr(self.cfg, "eval_mode", False)):
             if len(env_ids) > 0:
@@ -1053,38 +1090,46 @@ class MotionCommandCfg(CommandTermCfg):
 
     class_type: type = MotionCommand
 
-    dataset_txt: str = None  # "/home/xiechunyang/wt_ws/wt_wbc/dataset/g1-mimic-npz/dataset.txt"
+    # basic configs
     eval_mode: bool = False
-    # Evaluation: if True, pin env i to motion i (requires num_envs == number of motions loaded).
     fixed_eval_motion_ids: bool = False
     adaptive_sample: bool = True
     asset_name: str = MISSING
     max_motion_num: int = 999999
     resample_interval: int = 300000000000
     motion_file: str = MISSING
+    dataset_txt: str = None  # "/home/xiechunyang/wt_ws/wt_wbc/dataset/g1-mimic-npz/dataset.txt"
+
+    # reference motion obs configs
     anchor_body_name: str = MISSING
     body_names: list[str] = MISSING
-    log_save_path: str = "train_logs"
+    future_step_num = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    max_future_step = max(future_step_num)
+    
+    # randomization configs
     pose_range: dict[str, tuple[float, float]] = {}
     velocity_range: dict[str, tuple[float, float]] = {}
-
-    future_step_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
     joint_position_range: tuple[float, float] = (-0.52, 0.52)
-
+    
+    # adaptive sampling configs
     adaptive_kernel_size: int = 3
     adaptive_lambda: float = 0.8
-    adaptive_uniform_ratio: float = 0.1
+    motion_ratio = [0.2, 0.79, 0.01]  # 预留参数，暂时不使用 # 越来越难
+    adaptive_uniform_ratio: float = 0.5
     adaptive_alpha: float = 0.001
 
     failure_cap: bool = True
     failure_cap_beta: float = 200.0
+    failure_most_hard_cap_beta: float = 5000.0
 
+    # adaptive bins log configs
     # 每隔多少步导出一次 adaptive bins 概率和 bin->motion 反查映射，-1 表示不保存
-    fail_count_save_interval: int = 1000 * 24
+    log_save_path: str = "train_logs"
+    fail_count_save_interval: int = 500 * 24
     save_adaptive_bins: bool = True
     adaptive_bins_file_prefix: str = "adaptive_bins"
 
+    # visualizer configs
     anchor_visualizer_cfg: VisualizationMarkersCfg = FRAME_MARKER_CFG.replace(prim_path="/Visuals/Command/pose")
     anchor_visualizer_cfg.markers["frame"].scale = (0.2, 0.2, 0.2)
 
