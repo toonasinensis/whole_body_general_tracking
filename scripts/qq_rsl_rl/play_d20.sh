d1fbef2f12469d241242d7b91ba4602af08847f1
kiki <zhangsevenseven@gmail.com>
2026-06-17 16:19:18 +0800

    validated commands decomposition

--- scripts/qq_rsl_rl/play_d20.sh
+++ scripts/qq_rsl_rl/play_d20.sh
@@ -1,7 +1,7 @@
 python scripts/qq_rsl_rl/play.py \
 --task=Tracking-Flat-D20-v0 \
 --num_envs=16 \
---resume_path=logs/rsl_rl/d20_flat/0609_all/model_50000.pt \
+--resume_path=logs/rsl_rl/d20_flat/0609_all/model_103500.pt \
 --motion_file=/home/kiki/workspace/motion_data/mimic_data/d20v2/bones_seed \
 --motion_file_txt=dataset_txt/bones_seed/test.txt \
 --max_motion_num=160 \
